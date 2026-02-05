250784078dc3cb9f9fc4818d42205ba7f1ed64fd
Emirhan Topcuoglu <emirhantopcuoglu386@gmail.com>
2026-02-05 19:13:30 +0300

    chore: clean repository and update gitignore

---
+++ Migrations/20250503100806_AddIsArchivedToSubscription.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Subsy.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsArchivedToSubscription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsArchived",
+                table: "Subscriptions",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsArchived",
+                table: "Subscriptions");
+        }
+    }
+}
