d574e192fe782b67ae9354dacabe9d73ab6868a6
Emirhan Topcuoglu <emirhantopcuoglu386@gmail.com>
2026-02-08 16:05:43 +0300

    chore: remove repository implementation from Web layer

---
+++ Subsy.Web/Controllers/DashboardController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Subsy.Application.Subscriptions.Dashboard;
+using System.Security.Claims;
+
+namespace Subsy.Web.Controllers;
+
+[Authorize]
+public class DashboardController : Controller
+{
+    private readonly ISubscriptionDashboardService _dashboardService;
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public DashboardController(
+        ISubscriptionDashboardService dashboardService,
+        UserManager<IdentityUser> userManager)
+    {
+        _dashboardService = dashboardService;
+        _userManager = userManager;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        var dashboard = await _dashboardService.GetDashboardAsync(userId!);
+
+        return Json(dashboard); 
+    }
+}
